39bffc18aa3c4391daf68c591dca2bfa43540816
sujeong Kim <sujung0619@naver.com>
2023-10-14 23:25:43 +0900

    Create senior-web-service Repository.

---
+++ src/main/java/com/example/seniorwebservice/SeniorWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.seniorwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seniorwebservice/SeniorWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seniorwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
